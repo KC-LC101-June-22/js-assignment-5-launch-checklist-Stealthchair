4762b2daf9cf4b15b1e91bd691970021aba24114
Stealthchair <31829676+Stealthchair@users.noreply.github.com>
2022-09-09 12:30:49 -0500

    trying to JSON.stringify()

--- spec/grading-dom.spec.js
+++ spec/grading-dom.spec.js
@@ -36,8 +36,8 @@ describe ("GRADING DOM MANIPULATION TEST: ", function () {
         window.addEventListener('load', resolve);
           container = dom.window.document.body;
           list = screen.getByTestId(container, "faultyItems");
-          console.log("Style: " + list.style);
-          console.log("Computed Style: " + window.getComputedStyle(list));
+          console.log("Style: " + JSON.stringify(list.style));
+          console.log("Computed Style: " + JSON.stringify(window.getComputedStyle(list)));
           h2 = screen.getByTestId(container, "launchStatus");
           pilotStatus = screen.getByTestId(container, "pilotStatus");
           copilotStatus = screen.getByTestId(container, "copilotStatus");
